2581ab725f45e49d3e50feadc462c022788e9670
lcolladotor <lcolladotor@gmail.com>
2024-08-27 10:41:36 -0400

    Info about "devel"

--- R/2024-08-27.R
+++ R/2024-08-27.R
@@ -25,3 +25,25 @@ library("ggplot2")
 ## keyboard shortcut (on macOS, for winOS use control + enter)
 
 
+## After class question:
+## What does "devel" mean on your R Console's command prompt?
+##
+## Answer: it's the Git branch that I'm currently working on.
+## I have this automatically loaded on all my R sessions through the
+## ~/.Rprofile configuration file that you can edit with:
+usethis::edit_r_profile()
+
+## These are the specific lines that modify my R Console prompt:
+
+## From https://twitter.com/hadleywickham/status/1113542388033699840
+if (interactive()) {
+    if (!requireNamespace("prompt", quietly = TRUE)) {
+        cat('To install prompt use: remotes::install_github("gaborcsardi/prompt")\n')
+    } else {
+        prompt::set_prompt(prompt::prompt_git)
+    }
+}
+
+
+## For more on my Rprofile check
+## https://lcolladotor.github.io/bioc_team_ds/config-files.html#rprofile
