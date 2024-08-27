017f6929d0befc8f87e9cabeb65b66af586fab16
lcolladotor <lcolladotor@gmail.com>
2024-08-27 10:41:19 -0400

    From today's first class

---
+++ R/2024-08-27.R
@@ -0,0 +1,27 @@
+## Load the usethis package as an example
+library("usethis")
+
+## Create an R object called "leo"
+leo <- "hola"
+
+## Check under
+## Help -> Keyboard shortcuts help
+## the keyboard shortcut for executing code
+## from a selected line(s) on the console.
+##
+## Answer: It's command + enter on macOS (control + enter on winOS)
+
+## Install ggplot2 and usethis
+install.packages("ggplot2")
+install.packages("usethis")
+
+## Load ggplot2 in this R session
+library("ggplot2")
+
+## Let's open the help page for ggplot2::ggplot()
+?ggplot
+
+## Then execute code from the examples using the command + enter
+## keyboard shortcut (on macOS, for winOS use control + enter)
+
+
