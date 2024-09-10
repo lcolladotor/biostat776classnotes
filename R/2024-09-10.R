c31b23ad2045d0cf21d25f9353155f5aa4cdd085
lcolladotor <lcolladotor@gmail.com>
2024-09-10 10:40:28 -0400

    Add comments for today's class and remove code we didn't use

--- R/2024-09-10.R
+++ R/2024-09-10.R
@@ -7,14 +7,19 @@ outcomes <- tibble(
     outcome = rnorm(3 * 3, 3)
 )
 
+## Let's create a non-tidy table where id and visit are combined into a single
+## variable.
 outcomes_modified <- outcomes %>%
     mutate("id_visit" = paste0(id, "_", visit)) %>%
     select(-(id:visit))
 outcomes_modified
 
+## If we had started with non-tidy data, we would have had to clean it up
+## using mutate() and other functions as shown below.
 outcomes_modified %>%
     mutate(visit = gsub(".*_", "", id_visit), id = gsub("_.*", "", id_visit))
 
+## In any case, our "outcomes" table is tidy to begin with.
 print(outcomes)
 
 
@@ -26,29 +31,13 @@ subjects <- tibble(
 
 print(subjects)
 
+## Example on how to find all the functions from a given
+## R package
 help(package = "dplyr")
+## Example on how to find all functions currently loaded (aka from library calls)
+## that have a specific keyword in their name
+library("dplyr")
 apropos("join")
-
-## ------------------------------------------------------------------------------------------------------------------------
-#| echo: false
-#| out-width: '60%'
-#| fig-align: 'center'
-library(knitr)
-join_funcs <- data.frame(
-    func = c(
-        "`left_join()`",
-        "`right_join()`",
-        "`inner_join()`",
-        "`full_join()`"
-    ),
-    does = c(
-        "Includes all observations in the left data frame, whether or not there is a match in the right data frame",
-        "Includes all observations in the right data frame, whether or not there is a match in the left data frame",
-        "Includes only observations that are in both data frames",
-        "Includes all observations from both data frames"
-    )
-)
-knitr::kable(join_funcs, col.names = c("Function", "What it includes in merged data frame"))
 
 
 ## ------------------------------------------------------------------------------------------------------------------------
@@ -60,10 +49,12 @@ subjects
 left_join(x = outcomes, y = subjects, by = "id")
 full_join(x = outcomes, y = subjects, by = "id")
 
+## Are left_join() and full_join() giving us the same result in this case?
 identical(
     left_join(x = outcomes, y = subjects, by = "id"),
     full_join(x = outcomes, y = subjects, by = "id")
 )
+## Answer: yes. Both tables have info for subjects a, b, and c.
 
 ## ------------------------------------------------------------------------------------------------------------------------
 subjects <- tibble(
@@ -78,7 +69,11 @@ print(subjects)
 ## ------------------------------------------------------------------------------------------------------------------------
 left_join(outcomes, subjects, by = c("id", "visit"))
 
+## Do we get a different number of rows with inner_join() in this case than
+## with left_join()?
 inner_join(outcomes, subjects, by = c("id", "visit"))
+## Yes: not all id + visit combinations are observed in the right table, so
+## the intersection (inner_join) is going to be smaller.
 
 ## ------------------------------------------------------------------------------------------------------------------------
 subjects <- tibble(
@@ -94,10 +89,14 @@ subjects
 left_join(x = outcomes, y = subjects, by = c("id", "visit"))
 full_join(outcomes, subjects, by = c("id", "visit"))
 
+## Are left_join() and full_join() giving us the same result in this case?
 identical(
     left_join(x = outcomes, y = subjects, by = c("id", "visit")),
     full_join(outcomes, subjects, by = c("id", "visit"))
 )
+## Answer: yes. The right side table doesn't have any id + visit combinations
+## that the left table is missing, so left_join() which prioritizes the left
+## table will give the same result as full_join() in this case.
 
 ## ------------------------------------------------------------------------------------------------------------------------
 inner_join(x = outcomes, y = subjects, by = c("id", "visit"))
@@ -105,26 +104,6 @@ inner_join(x = outcomes, y = subjects, by = c("id", "visit"))
 
 ## ------------------------------------------------------------------------------------------------------------------------
 right_join(x = outcomes, y = subjects, by = c("id", "visit"))
-
-
-## ------------------------------------------------------------------------------------------------------------------------
-# Create first example data frame
-df1 <- data.frame(
-    ID = 1:3,
-    X1 = c("a1", "a2", "a3")
-)
-# Create second example data frame
-df2 <- data.frame(
-    ID = 2:4,
-    X2 = c("b1", "b2", "b3")
-)
-
-
-## ------------------------------------------------------------------------------------------------------------------------
-options(width = 120)
-sessioninfo::session_info()
-
-
 
 
 
@@ -199,259 +178,3 @@ penguins %>%
     ggplot(mapping = aes(x = flipper_length_mm, y = bill_length_mm, color = species)) +
     geom_point() +
     geom_smooth(method = "lm")
-
-
-## ------------------------------------------------------------------------------------------------------------------------
-#| fig-width: 9
-#| fig-cap: "Scatterplot of PM2.5 and nocturnal symptoms by BMI category"
-#| message: false
-g +
-    geom_point() +
-    geom_smooth(method = "lm") +
-    facet_grid(. ~ bmicat)
-
-
-## ------------------------------------------------------------------------------------------------------------------------
-#| fig-cap: "Modifying point color with a constant"
-g + geom_point(color = "steelblue", size = 4, alpha = 1 / 2)
-
-
-## ------------------------------------------------------------------------------------------------------------------------
-#| fig-cap: "Mapping color to a variable"
-g + geom_point(aes(color = bmicat), size = 4, alpha = 1 / 2)
-
-
-## ------------------------------------------------------------------------------------------------------------------------
-#| fig-cap: "Customizing a smoother"
-#| message: false
-g +
-    geom_point(aes(color = bmicat),
-        size = 2,
-        alpha = 1 / 2
-    ) +
-    geom_smooth(
-        linewidth = 4,
-        linetype = 3,
-        method = "lm",
-        se = FALSE
-    )
-
-
-## ------------------------------------------------------------------------------------------------------------------------
-#| fig-cap: "Modifying the theme for a plot"
-g +
-    geom_point(aes(color = bmicat)) +
-    theme_bw(base_family = "Times")
-
-
-## ------------------------------------------------------------------------------------------------------------------------
-# try it yourself
-
-library("palmerpenguins")
-penguins
-
-
-## ------------------------------------------------------------------------------------------------------------------------
-#| fig-cap: "Modifying plot labels"
-g +
-    geom_point(aes(color = bmicat)) +
-    labs(title = "MAACS Cohort") +
-    labs(
-        x = expression("log " * PM[2.5]),
-        y = "Nocturnal Symptoms"
-    )
-
-
-## ------------------------------------------------------------------------------------------------------------------------
-#| fig-cap: "Time series plot with base graphics"
-testdat <- data.frame(
-    x = 1:100,
-    y = rnorm(100)
-)
-testdat[50, 2] <- 100 ## Outlier!
-plot(testdat$x,
-    testdat$y,
-    type = "l",
-    ylim = c(-3, 3)
-)
-
-
-## ------------------------------------------------------------------------------------------------------------------------
-#| fig-cap: "Time series plot with default settings"
-g <- ggplot(testdat, aes(x = x, y = y))
-g + geom_line()
-
-
-## ------------------------------------------------------------------------------------------------------------------------
-#| fig-cap: "Time series plot with modified ylim"
-g +
-    geom_line() +
-    ylim(-3, 3)
-
-
-## ------------------------------------------------------------------------------------------------------------------------
-#| fig-cap: "Time series plot with restricted y-axis range"
-g +
-    geom_line() +
-    coord_cartesian(ylim = c(-3, 3))
-
-
-## ------------------------------------------------------------------------------------------------------------------------
-#| eval: false
-## ## Install bbplot
-## remotes::install_github("bbc/bbplot")
-
-
-## ------------------------------------------------------------------------------------------------------------------------
-#| message: false
-#| fig-width: 13
-## Basic ggplot2 object with our data
-g <- maacs %>%
-    ggplot(aes(logpm25, NocturnalSympt))
-
-## A plot we made before, but this time without the SE lines
-g +
-    geom_point() +
-    geom_smooth(method = "lm", se = FALSE) +
-    facet_grid(. ~ bmicat)
-
-## Now let's add bbplot::bbc_style()
-g +
-    geom_point() +
-    geom_smooth(method = "lm", se = FALSE) +
-    facet_grid(. ~ bmicat) +
-    bbplot::bbc_style()
-
-
-## ------------------------------------------------------------------------------------------------------------------------
-#| message: false
-#| fig-width: 13
-g +
-    geom_point() +
-    geom_smooth(colour = "#1380A1", method = "lm", se = FALSE) +
-    facet_grid(. ~ bmicat) +
-    bbplot::bbc_style() +
-    labs(
-        title = "Child asthma's link to air quality worsens in overweight children",
-        subtitle = "Number of days with symptoms vs PM2.5 by weight group"
-    )
-
-
-## ------------------------------------------------------------------------------------------------------------------------
-#| message: false
-#| fig-width: 13
-g +
-    geom_smooth(aes(colour = bmicat), method = "lm", se = FALSE, linewidth = 2) +
-    scale_colour_manual(values = c("#FAAB18", "#1380A1")) +
-    bbplot::bbc_style() +
-    labs(
-        title = "Child asthma's link to air quality worsens in overweight children",
-        subtitle = "Number of days with symptoms vs PM2.5 by weight group"
-    )
-
-
-## ------------------------------------------------------------------------------------------------------------------------
-#| eval: false
-## ## Install ThemePark from GitHub
-## remotes::install_github("MatthewBJane/theme_park")
-
-
-## ------------------------------------------------------------------------------------------------------------------------
-#| message: false
-#| fig-width: 13
-## Barbie-inspired theme
-g +
-    geom_smooth(aes(colour = bmicat), method = "lm", se = FALSE, linewidth = 2) +
-    scale_colour_manual(values = c("#FAAB18", "#1380A1")) +
-    ThemePark::theme_barbie() +
-    labs(
-        title = "Child asthma's link to air quality worsens in overweight children",
-        subtitle = "Number of days with symptoms vs PM2.5 by weight group"
-    )
-
-## Oppenheimer-inspired theme
-g +
-    geom_smooth(aes(colour = bmicat), method = "lm", se = FALSE, linewidth = 2) +
-    scale_colour_manual(values = c("#FAAB18", "#1380A1")) +
-    ThemePark::theme_oppenheimer() +
-    labs(
-        title = "Child asthma's link to air quality worsens in overweight children",
-        subtitle = "Number of days with symptoms vs PM2.5 by weight group"
-    )
-
-
-## ------------------------------------------------------------------------------------------------------------------------
-#| eval: false
-## ## Install ggthemes from CRAN
-## install.packages("ggthemes")
-
-
-## ------------------------------------------------------------------------------------------------------------------------
-#| message: false
-## Your favorite statistics class theme ;)
-## I bet that you could fool a few people into thinking
-## that you are not using R ^_^'
-g +
-    geom_smooth(aes(colour = bmicat), method = "lm", se = FALSE, linewidth = 2) +
-    scale_colour_manual(values = c("#FAAB18", "#1380A1")) +
-    ggthemes::theme_stata() +
-    labs(
-        title = "Child asthma's link to air quality worsens in overweight children",
-        subtitle = "Number of days with symptoms vs PM2.5 by weight group"
-    )
-
-
-## ------------------------------------------------------------------------------------------------------------------------
-#| message: false
-## Save our plot into an object
-g_complete <- g +
-    geom_point(aes(colour = bmicat)) +
-    geom_smooth(aes(colour = bmicat), method = "lm", se = FALSE, linewidth = 2) +
-    scale_colour_manual(values = c("#FAAB18", "#1380A1"))
-
-## Make it interactive with plotly::ggplotly()
-plotly::ggplotly((g_complete))
-
-
-## ------------------------------------------------------------------------------------------------------------------------
-#| eval: false
-## ## Install colorblindr from GitHub
-## remotes::install_github("clauswilke/colorblindr")
-
-
-## ------------------------------------------------------------------------------------------------------------------------
-#| message: false
-colorblindr::cvd_grid(g_complete)
-
-
-## ------------------------------------------------------------------------------------------------------------------------
-cutpoints <- quantile(maacs$logno2_new, seq(0, 1, length = 4), na.rm = TRUE)
-
-
-## ------------------------------------------------------------------------------------------------------------------------
-maacs$no2tert <- cut(maacs$logno2_new, cutpoints)
-
-
-## ------------------------------------------------------------------------------------------------------------------------
-## See the levels of the newly created factor variable
-levels(maacs$no2tert)
-
-
-## ----fig.cap="PM2.5 and nocturnal symptoms by BMI category and NO2 tertile",fig.width=9, fig.height=5--------------------
-## Setup ggplot with data frame
-g <- maacs %>%
-    ggplot(aes(logpm25, NocturnalSympt))
-
-## Add layers
-g + geom_point(alpha = 1 / 3) +
-    facet_grid(bmicat ~ no2tert) +
-    geom_smooth(method = "lm", se = FALSE, col = "steelblue") +
-    theme_bw(base_family = "Avenir", base_size = 10) +
-    labs(x = expression("log " * PM[2.5])) +
-    labs(y = "Nocturnal Symptoms") +
-    labs(title = "MAACS Cohort")
-
-
-## ------------------------------------------------------------------------------------------------------------------------
-options(width = 120)
-sessioninfo::session_info()
