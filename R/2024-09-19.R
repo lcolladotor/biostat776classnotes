2de1f21a24821f154fb4253536f9165484106a73
lcolladotor <lcolladotor@gmail.com>
2024-09-19 08:36:28 -0400

    Start class notes for today. Revisit recyling from https://lcolladotor.github.io/jhustatcomputing/posts/08-managing-data-frames-with-tidyverse/index.html#creating-a-tibble

---
+++ R/2024-09-19.R
@@ -0,0 +1,29 @@
+## Recycling review
+## https://lcolladotor.github.io/jhustatcomputing/posts/08-managing-data-frames-with-tidyverse/index.html#creating-a-tibble
+
+library("tibble")
+tibble(
+    a = 1:5,
+    b = 6:10,
+    c = 1,
+    z = (a + b)^2 + c
+)
+## Q1: Which column got recycled?
+
+set.seed(20240919)
+data.frame(
+    x = 1:10,
+    y = rnorm(10),
+    in_group = c(TRUE, FALSE)
+)
+## Q2a: Will this work?
+## Q3a: How ill in_group look like?
+
+set.seed(20240919)
+data.frame(
+    x = 1:10,
+    y = rnorm(10),
+    in_group = c(TRUE, FALSE, NA)
+)
+## Q2b: Will this work?
+## Q3b: How ill in_group look like?
