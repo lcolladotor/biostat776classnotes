6f0e9213a291ef193e603c1fa372dce09250e25a
lcolladotor <lcolladotor@gmail.com>
2024-09-19 11:54:25 -0400

    Add notes from questions during today's class with some comments

--- R/2024-09-19.R
+++ R/2024-09-19.R
@@ -29,6 +29,19 @@ data.frame(
 ## Q3b: How ill in_group look like?
 
 
+x <- data.frame(foo = 1:4,
+    bar = c(T, T, F, F),
+    leo = 2:5)
+data.matrix(x[, sapply(x, is.integer)])
+
+
+x <- 1:3
+names(x) <- c("New York", "Seattle", "Los Angeles")
+x
+x["Seattle"]
+x[["Seattle"]]
+
+c("morning" = 5, "night" = 8)
 
 ## Review %in% operator:
 ## it returns a vector with length equal to the
@@ -36,9 +49,33 @@ data.frame(
 "b" %in% letters
 letters %in% "b"
 
+c("a", "ch", "b") %in% letters
+ifelse(c("a", "ch", "b") %in% letters, "woo", "boo")
+
 ## %in% is not to be confused with the in from:
 ##     for(i in vector)
 ##           ^
+
+for (i in 1:10) {
+    print(i)
+    Sys.sleep(1)
+}
+
+## define the loop to iterate over
+x <- c("a", "b", "c", "d")
+
+## create for loop
+for (i in 1:2) {
+    print(x[i])
+}
+for (i in seq_along(x)) {
+    print(x[i])
+}
+
+x <- c(x, "leo", "class")
+for (i in seq_along(x)) {
+    print(x[i])
+}
 
 ## Explicit vs implicit printing in a for() loop
 for (i in 1:10) {
@@ -50,3 +87,53 @@ for (i in 1:10) {
     i
 }
 i
+
+## Doesn't have to be "i"
+for (RememberToVote in x) {
+    print(RememberToVote)
+}
+
+
+## Example with more than one computation and more than one
+## print() call inside a for() loop.
+for (i in 1:10) {
+    print(i)
+    print(i^2)
+    print(i^2 + i)
+}
+
+
+## Example where "hello" only exists inside the function f()
+f <- function() {
+    # this is the function body
+    hello <- "Hello, world!\n"
+    cat(hello)
+}
+f()
+hello ## This will return an error
+
+## Set i on the global environment via a for() loop
+for (i in 1:10) {
+    i
+}
+i ## Q: What is the value of "i" at the end of the for() loop?
+
+## f() with an argument
+f <- function(num) {
+    for (i in seq_len(num)) {
+        hello <- "Hello, world!\n"
+        cat(hello)
+    }
+}
+f(3)
+i ## Q: What is the value of "i" going to be?
+
+f <- function(num) {
+    for (internal_i in seq_len(num)) {
+        hello <- "Hello, world!\n"
+        cat(hello)
+    }
+}
+f(3)
+i ## Q: What is the value of "i" going to be?
+
