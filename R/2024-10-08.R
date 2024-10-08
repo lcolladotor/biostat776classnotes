bd08730f5ab0fde8c56bfaf6dad8bf8a8a54f039
lcolladotor <lcolladotor@gmail.com>
2024-10-08 08:56:45 -0400

    Start notes for today's class on forcats 🐈‍⬛🐈 and factors

---
+++ R/2024-10-08.R
@@ -0,0 +1,5 @@
+## Starting this exercise
+## https://lcolladotor.github.io/jhustatcomputing/posts/22-working-with-factors/#fct_reorder
+library("tidyverse")
+library("palmerpenguins")
+penguins %>% glimpse()
