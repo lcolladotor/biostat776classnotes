a8dcffc49d55b5f946e526ad998811e58573805e
lcolladotor <lcolladotor@gmail.com>
2025-08-26 12:21:25 -0400

    Add comments from today's class

---
+++ R/2025-08-26.R
@@ -0,0 +1,22 @@
+## List R packages in this session
+sessioninfo::session_info()
+
+## How many R packages are available on CRAN?
+available.packages() |> nrow()
+
+## How many R packages are available on Bioconductor?
+BiocManager::available() |> length() - available.packages() |> nrow()
+
+## Cuantos paquetes de R hay en total?
+## How many R packages are there in total?
+
+## R code that is not formatted
+a <- function(hola) {
+    print(hola)
+}
+
+## Show me an example of a for loop in R
+for (i in 1:5) {
+    print(i)
+}
+?print
