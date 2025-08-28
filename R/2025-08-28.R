c14aa9912e763c99ddb45a8541a888f6281d9cd4
lcolladotor <lcolladotor@gmail.com>
2025-08-28 10:41:10 -0400

    Notes from today's class

---
+++ R/2025-08-28.R
@@ -0,0 +1,13 @@
+print("hola!")
+stop("error that I don't understand")
+
+sessioninfo::session_info()
+
+## Copy the code from lines 1 to 4 above
+## then run reprex::reprex()
+##
+## then paste the content to
+## https://github.com/lcolladotor/jhustatcomputing/issues/10
+
+## Links from the class today:
+## https://www.one-tab.com/page/J5NAoHiYRC-4JfEY-3agOQ
