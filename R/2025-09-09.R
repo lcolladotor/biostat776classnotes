6942c93684631f5017db0c378a018e0ec202ebaa
lcolladotor <lcolladotor@gmail.com>
2025-09-09 10:34:44 -0400

    Notes from class today

---
+++ R/2025-09-09.R
@@ -0,0 +1,8 @@
+## Show all fucntions from the dplyr package
+help(package = "dplyr")
+
+## Student websites from today:
+## https://www.one-tab.com/page/QvR9-JJERtKKogWXPZTKWQ
+
+## Links from the class today:
+## https://www.one-tab.com/page/jUtnRu8qRjSoZHWB-Sq4gQ
