c16ce1ec1ecd66e7d3cf090e2c0ba4e4dbb067f0
lcolladotor <lcolladotor@gmail.com>
2025-10-07 09:00:15 -0400

    Start class notes today with info about how to use cowsay::say("fortune", by = "cow") in your .Rprofile

---
+++ R/2025-10-07.R
@@ -0,0 +1,26 @@
+## Install both cowsay and fortunes so we
+## can generate a random fortune from a cow
+install.packages(c("cowsay", "fortunes"))
+
+## Example using the "cowsay" package
+library("cowsay")
+library("fortunes")
+cowsay::say("fortune", by = "cow")
+
+## Do you want a fortune said by a cow every
+## time you start R?
+
+## If your answer is yes, run the following line
+usethis::edit_r_profile()
+
+## Add these lines to your .Rprofile
+if (
+    !requireNamespace("cowsay", quietly = TRUE) |
+        !requireNamespace("fortunes", quietly = TRUE)
+) {
+    cat(
+        'To install cowsay and fortunes use: install.packages(c("cowsay", "fortunes"))\n'
+    )
+} else {
+    cowsay::say("fortune", by = "cow")
+}
