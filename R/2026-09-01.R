1644bb72cfdfa4562830f462716a66effb67ef5e
lcolladotor <lcolladotor@gmail.com>
2026-09-01 10:44:08 -0400

    Notes from 2026-09-01

---
+++ R/2026-09-01.R
@@ -0,0 +1,12 @@
+## Example code auto-formatted with Air
+leo <- "this is hard to read"
+foo <- function(x, y) {
+  x * y
+}
+
+## Install the ggplot2 package
+install.packages("ggplot2")
+
+## Load the ggplot2 pkg
+library("ggplot2")
+help(package = "ggplot2")
