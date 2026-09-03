b47a5a564726d78a3ffa5f87d28482a96a2ed5a7
lcolladotor <lcolladotor@gmail.com>
2026-09-03 09:18:01 -0400

    I copy paste the R commands from https://github.com/lcolladotor/jhustatcomputing/blob/main/posts/03-introduction-to-gitgithub/index.R

--- R/2026-09-03.R
+++ R/2026-09-03.R
@@ -1,2 +1,39 @@
 ## I created this file with:
 # usethis::use_r("2026-09-03")
+
+## -----------------------------------------------------------------------------------------
+#| label: versioncontrol
+#| echo: false
+#| fig-cap: 'How not to use GitHub [image from PhD Comics]'
+#| out-width: '80%'
+knitr::include_graphics("../../images/phdversioncontrol.gif")
+
+
+## -----------------------------------------------------------------------------------------
+#| label: happygitwithr
+#| echo: false
+#| fig-cap: 'A screenshot of the Happy Git with R online book from Jenny Bryan'
+#| out-width: '80%'
+knitr::include_graphics("../../images/happygitwithr.png")
+
+
+## -----------------------------------------------------------------------------------------
+#| eval: false
+# ## Install "usethis" if you don't have it already
+# if (!requireNamespace("usethis", quietly = TRUE)) {
+#     install.packages("usethis")
+# }
+#
+# usethis::create_project("~/Desktop/biostat776classnotes")
+#
+# ## Then in your new Positron window / RStudio project, run the following:
+# usethis::use_git()
+# usethis::use_github()
+
+## -----------------------------------------------------------------------------------------
+#| eval: false
+# install.packages("reprex")
+
+## -----------------------------------------------------------------------------------------
+options(width = 120)
+sessioninfo::session_info()
