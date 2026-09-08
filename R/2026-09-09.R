c8738da6c943f099c02886fc9de71f8d1300687b
lcolladotor <lcolladotor@gmail.com>
2026-09-08 10:32:07 -0400

    getwd() example

--- R/2026-09-09.R
+++ R/2026-09-09.R
@@ -2,3 +2,7 @@ citation("rmarkdown")
 citation("rmarkdown")[1]
 
 knitr::write_bib("rmarkdown", file = "my-refs.bib")
+
+## From https://lcolladotor.github.io/jhustatcomputing/posts/07-reading-and-writing-data/
+getwd()
+list.files()
