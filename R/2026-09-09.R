0a75069b0401e3c5e6c59d3b0323ecf7ee46cfe0
lcolladotor <lcolladotor@gmail.com>
2026-09-08 10:33:01 -0400

    Update the location of my-refs.bib using here()

--- R/2026-09-09.R
+++ R/2026-09-09.R
@@ -1,7 +1,7 @@
 citation("rmarkdown")
 citation("rmarkdown")[1]
 
-knitr::write_bib("rmarkdown", file = "my-refs.bib")
+knitr::write_bib("rmarkdown", file = here::here("data", "my-refs.bib"))
 
 ## From https://lcolladotor.github.io/jhustatcomputing/posts/07-reading-and-writing-data/
 getwd()
