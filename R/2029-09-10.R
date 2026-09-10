acd3b2ce80eab811190f9125eb8d65d6acbf0429
lcolladotor <lcolladotor@gmail.com>
2026-09-10 09:14:31 -0400

    demo about readLines

--- R/2029-09-10.R
+++ R/2029-09-10.R
@@ -1,2 +1,8 @@
 ## We are resuming lecture 07 from
 ## https://lcolladotor.github.io/jhustatcomputing/posts/07-reading-and-writing-data/#reading-data-in-base-r
+
+library("here")
+x <- readLines(here("data", "team_standings.csv"))
+
+length(x)
+x[1]
