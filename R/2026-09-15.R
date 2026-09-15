c885156ab58a69016ff83b2f8da9eea529858120
lcolladotor <lcolladotor@gmail.com>
2026-09-15 10:38:39 -0400

    Inspecting code prior to pivot_wider() and example use of apropos()

--- R/2026-09-15.R
+++ R/2026-09-15.R
@@ -30,3 +30,24 @@ relig_income %>%
   ) -> income_tidy_right
 
 identical(income_tidy, income_tidy_right)
+
+
+## Prior to pivot_wider()
+relig_income %>%
+  pivot_longer(-religion, names_to = "income", values_to = "respondents") %>%
+  mutate(religion = factor(religion), income = factor(income)) %>%
+  group_by(income) %>%
+  summarize(total_respondents = sum(respondents))
+
+relig_income %>%
+  pivot_longer(-religion, names_to = "income", values_to = "respondents") %>%
+  mutate(religion = factor(religion), income = factor(income)) %>%
+  group_by(income) %>%
+  summarize(total_respondents = sum(respondents)) %>%
+  pivot_wider(
+    names_from = "income",
+    values_from = "total_respondents"
+  )
+
+## Finding related functions by a suffix
+apropos("_join")
